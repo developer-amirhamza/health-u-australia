5ed1071bb78e52074c1006c41a6d4c685d07111c
Amir Hamza <amirhamza27940@gmail.com>
2026-05-17 18:57:07 +1000

    http error solved

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
   output: "export",
+  trailingSlash: true,
   images:{
     unoptimized:true,
   }
