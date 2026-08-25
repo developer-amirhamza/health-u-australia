2fe361a0831f5451373e27dee9906fe95ce579e4
Amir Hamza <amirhamza27940@gmail.com>
2026-08-25 16:00:25 +1000

    error

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express();
+
+
+
+const PORT = Number(process.env.PORT) || 5000;
+
+
+app.use(express.json());
+app.get("/", (req,res)=>{
+    res.send("<center> <h1>Welcome to Health U Australia</h1> </center>")
+})
+
+
+app.listen(PORT, '0.0.0.0',()=>{
+    console.log(`The server is running at http://0.0.0.0:${PORT}`)
+})
