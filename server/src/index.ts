b5810306323ca71251301ccaeaca4283db800889
Amir Hamza <amirhamza27940@gmail.com>
2026-08-26 16:30:33 +1000

    server setup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
-import express from "express"
+import app from "./app"
 import dotenv from "dotenv"
 
 dotenv.config()
-const app = express();
+
 
 
 
 const PORT = Number(process.env.PORT) || 5000;
 
 
-app.use(express.json());
+
 app.get("/", (req,res)=>{
     res.send("<center> <h1>Welcome to Health U Australia</h1> </center>")
 })
