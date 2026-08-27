164e663242c5bb685e3291919010584e3bc587e4
Amir Hamza <amirhamza27940@gmail.com>
2026-08-27 16:02:25 +1000

    error showing

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,3 +26,5 @@ async function main() {
 }
 
 main().finally(() => prisma.$disconnect());
+
+
--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -3,7 +3,7 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 import 'dotenv/config'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -13,7 +13,7 @@ const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
-};
+}
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
