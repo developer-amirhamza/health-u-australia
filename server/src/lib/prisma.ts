b2d206f9a10ecf28bb80a809a99a6d4190d59108
Amir Hamza <amirhamza27940@gmail.com>
2026-09-03 17:57:32 +1000

    server

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -3,7 +3,7 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 import 'dotenv/config'
-import { PrismaClient } from '../generated/prisma/client.js'
+import { PrismaClient } from '@prisma/client'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
